a2a0c9a69e037e7b787fc03aef19723771e3dc29
Julie <j49411@gmail.com>
2020-03-27 19:52:46 +0000

    chore: initialise project files

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-console.log('Start')
+// console.log('Start')
 
-setTimeout(() => {
-  console.log('3 secs timer')
-}, 3000)
+// setTimeout(() => {
+//   console.log('3 secs timer')
+// }, 3000)
 
-setTimeout(() => {
-  console.log('0 secs timer')
-}, 0)
+// setTimeout(() => {
+//   console.log('0 secs timer')
+// }, 0)
 
-console.log('End')
+// console.log('End')
